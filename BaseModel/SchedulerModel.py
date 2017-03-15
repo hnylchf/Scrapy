5a92fc4b44e402fb8d23baa0c052fc8c1e4a821d
hnylchf <hnylchf@vip.qq.com>
2017-03-15 15:15:41 +0800

    编码

--- BaseModel/SchedulerModel.py
+++ BaseModel/SchedulerModel.py
@@ -1,1 +1,15 @@
-#!/usr/bin/python# -*- coding:utf-8 -*-from abc import ABCMeta, abstractmethodclass SchedulerModel(object):    __metaclass__ = ABCMeta    @abstractmethod    def run(self):pass    @abstractmethod    def add_event(self):pass
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+from abc import ABCMeta, abstractmethod
+
+class SchedulerModel(object):
+
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def run(self):pass
+
+    @abstractmethod
+    def add_event(self):pass
+
+
--- Util/ConfigUtil.py
+++ Util/ConfigUtil.py
@@ -1,15 +1,1 @@
-#!/usr/bin/python
-# -*- coding:utf-8 -*-
-import ConfigParser
-import os
-
-
-def getConfig(section, key):
-    try:
-        config = ConfigParser.ConfigParser()
-        # path = os.path.split(os.path.realpath(__file__))[0] + '/config/config.conf'
-        config.read("../config/config.conf")
-        return config.get(section, key)
-    except Exception,e:
-        print e
-
+#!/usr/bin/python# -*- coding:utf-8 -*-import ConfigParserimport osdef getConfig(section, key):    try:        config = ConfigParser.ConfigParser()        # path = os.path.split(os.path.realpath(__file__))[0] + '/config/config.conf'        config.read("../config/config.conf")        return config.get(section, key)    except Exception,e:        print e
